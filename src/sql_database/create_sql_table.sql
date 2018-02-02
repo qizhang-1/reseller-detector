947500073363bd6e6ae8b459f41cd1c927804511
qizhang-1 <bl1008@gmail.com>
2018-02-02 10:07:41 -0500

    sql database modification

---
+++ src/sql_database/create_sql_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE blacklist (
+    user_id       varchar(255),
+    order_number  varchar(255),
+    cell          varchar(255),
+    ipv4          varchar(255),
+    en0           varchar(255),
+    credit_card   varchar(255)
+);
